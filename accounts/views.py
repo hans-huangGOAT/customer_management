5fdad043e71ea1dc28ddfe6d6176ad60aac06f9f
Hans <fb_hans@outlook.com>
2020-12-22 22:35:38 +0800

    Functionalize update and delete button

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 
 # Create your views here.
 
 def home(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().order_by('-date_created')
     customers = Customer.objects.all()
-
-    total_customers = customers.count()
 
     total_orders = orders.count()
     delivered = orders.filter(status='Delivered').count()
@@ -27,7 +26,6 @@ def home(request):
 
 
 def products(request):
-
     products = Product.objects.all()
 
     context = {
@@ -48,3 +46,48 @@ def customer(request, pk):
         'order_count': order_count
     }
     return render(request, 'accounts/customer.html', context)
+
+
+def create_order(request):
+    form = OrderForm()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/order_form.html', context)
+
+
+def update_order(request, pk):
+    order = Order.objects.get(pk=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/order_form.html', context)
+
+
+def delete_order(request, pk):
+    order = Order.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {
+        'item': order
+    }
+    return render(request, 'accounts/delete.html', context)
